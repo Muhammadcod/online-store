53231748ff018eb8e71ec8eee8dadd4b2ef9c029
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-01-08 22:10:56 +0100

    feat: Cart action

---
+++ src/store/actions/cart.js
@@ -0,0 +1,29 @@
+// import firebase from 'firebase'
+import { RECIEVE_CART } from './actionTypes'
+
+export function receiveCart(cart) {
+  return {
+    type: RECIEVE_CART,
+    cart,
+  }
+}
+
+export function getCart() {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase()
+    const db = firebase.firestore()
+
+    db.collection('cart')
+      .get()
+      .then((snapshot) => {
+        const cart = []
+
+        snapshot.forEach((doc) => {
+          const currentID = doc.id
+          const appObj = { ...doc.data(), id: currentID }
+          cart.push(appObj)
+        })
+        dispatch(receiveCart(cart))
+      })
+  }
+}
